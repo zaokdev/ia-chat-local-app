e4cc77e85daa9ff4679776c0883a1b94202f0d02
Kevin Zapata <148372120+KevinZA0K@users.noreply.github.com>
2024-10-15 23:44:17 -0600

    First Commit

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,9 @@
+function App(): JSX.Element {
+  return (
+    <>
+      <div className="dark:bg-zinc-800 h-screen dark:text-white">Aquí voy a crear la app uwu</div>
+    </>
+  )
+}
+
+export default App
